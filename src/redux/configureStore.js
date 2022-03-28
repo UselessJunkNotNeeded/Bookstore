89bfc8c2ac0fc4c806d795f0c35ea207d3b919f5
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-03-28 22:58:12 +0530

    configured store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducer } from 'react-redux';
+
+import bookReducer from './books/books';
+import categoryReducer from './categories/categories';
+
+const reducer = combineReducer({
+  bookReducer,
+  categoryReducer,
+});
+
+const store = createStore(reducer);
+
+export default store;
